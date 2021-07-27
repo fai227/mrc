44f9f2ee78ca53b8c28fb5662644a2a83e7b1f8e
kogepanh <k.hosono1209@gmail.com>
2021-07-27 19:23:21 +0900

    #2 fix scripts

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 git pull origin main
+yarn install --production
 
-yarn install
-
-yarn build
-
-ps aux | grep node | grep -v grep | awk '{ print "kill -9", $2 }' | sh
-
-forever start -c 'yarn start' ./
+if [ -z "`ps aux | grep "next build" | grep -v grep`" ]; then
+  yarn build
+  ps aux | grep node | grep -v grep | awk '{ print "kill -9", $2 }' | sh
+  forever start -c 'yarn start' ./
+fi
 
 exit 0
