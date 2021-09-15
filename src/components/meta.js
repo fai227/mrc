4e58194a1c79d2877e8a955091d43e78c0aa9e7c
kogepanh <k.hosono1209@gmail.com>
2021-09-15 18:22:45 +0900

    #45 update font and name

--- src/components/meta.js
+++ src/components/meta.js
@@ -33,6 +33,7 @@ function Meta() {
       <meta property="og:title" content={site_title} />
       <meta property="og:description" content={description} />
       <meta name="twitter:card" content="summary_large_image" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
     </Head>
   );
 }
