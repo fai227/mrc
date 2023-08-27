1dc2e5700d52dd8399abaddc04d9af9fd7d1b00f
ss1119 <y.sasaki111919@gmail.com>
2023-08-27 14:05:04 +0900

    fix topics

--- src/pages/topics/[year].js
+++ src/pages/topics/[year].js
@@ -1,16 +1,15 @@
 import Head from "next/head";
 import { useRouter } from "next/dist/client/router";
 
-import { getLatestTopic, getAllTopics } from "../../lib/api";
+import { getAllTopcis, getTopic, getEnAllPublications, getEnPublication } from "../../lib/api";
 import { J_SITE_TITLE, E_SITE_TITLE } from "../../lib/constants";
 import markdownToHtml from "../../lib/markdownToHtml";
 import Layout from "../../components/layout";
 import Content from "../../components/content";
-import List from "../../components/list";
 import PageTitle from "../../components/pageTitle";
 import Sidebar from "../../components/sidebar";
 
-function Index({ body, jtitle, etitle, items }) {
+export default function Topic({ items, jtitle, etitle, body }) {
   const { locale } = useRouter();
   const site_title = locale === "ja-JP" ? J_SITE_TITLE : E_SITE_TITLE;
   const page_title = locale === "ja-JP" ? jtitle : etitle;
@@ -32,18 +31,30 @@ function Index({ body, jtitle, etitle, items }) {
           <Sidebar items={items} title={sidebar_title} type="topics" />
         </div>
       </div>
-      <List items={items} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const latestItem = await getLatestTopic();
-  const body = await markdownToHtml(latestItem.fields.body);
-  const items = await getAllTopics();
-  return {
-    props: { body: body, jtitle: latestItem.fields.title, etitle: latestItem.fields.etitle, items: items },
-  };
+export async function getStaticPaths({ locales }) {
+  const items = await getAllTopcis();
+  const paths = [];
+  items.map((item) => {
+    paths.push({
+      params: { year: item.fields.year.toString() },
+      locale: "ja-JP",
+    });
+    paths.push({
+      params: { year: item.fields.year.toString() },
+      locale: "en",
+    });
+  });
+
+  return { paths, fallback: false };
 }
 
-export default Index;
+export async function getStaticProps({ params }) {
+  const items = await getAllTopics();
+  const item = await getTopic(params.year);
+  const body = await markdownToHtml(item.fields.body);
+  return { props: { items: items, jtitle: item.fields.title, etitle: item.fields.etitle, body: body } };
+}
